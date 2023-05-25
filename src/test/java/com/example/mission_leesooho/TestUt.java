7fc28f71bcb9271816f4b0ddc216f50793439a0f
SoohoLee <hyeri0603@naver.com>
2023-05-25 23:55:05 +0900

    젠킨스 테스트

--- src/test/java/com/example/mission_leesooho/TestUt.java
+++
@@ -1,7 +0,0 @@
-package com.example.mission_leesooho;
-
-public class TestUt {
-//    public static boolean setFieldValue(Object o, String fieldName, Object value) {
-//        return Ut.reflection.setFieldValue(o, fieldName, value);
-//    }
-}
